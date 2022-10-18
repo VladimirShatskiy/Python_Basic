e3904b5834439ede0abf80a39e0e766940c39585
Владимир Шатский <vshatsky@nissan.ru>
2022-10-18 07:49:34 +0000

    Update main.py

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+alfa = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя    "
+
+word = input("Введи слово для шифрования:")
+str = []
+
+str = [alfa[alfa.index(word[i]) + 3] if word[i] != "" else "" for i in range(len(word))]
+
+for i in str:
+  print(i , end ="" )
