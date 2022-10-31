4f7a143b38dfc6cd82362efe002b45f1b02c7852
Владимир Шатский <vshatsky@nissan.ru>
2022-10-31 06:42:06 +0000

    Update main.py

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,20 +1,14 @@
-new_list = []
-def summ(*qwer):
-    fun_sum(list(qwer)[0])
-    print(sum(new_list))
-
 def fun_sum(list_num):
-    for i_num in list_num:
-        if isinstance(i_num, int):
-            new_list.append(i_num)
-        elif isinstance(i_num, list):
-            fun_sum(i_num)
-    return
+  summ = 0
+  for i_num in (list_num):
+    if isinstance(i_num, int):
+      summ += i_num
+    elif isinstance(i_num, list):
+      summ += fun_sum(i_num)
+  return summ
 
 
-
-
-summ([[1, 2, [3]], [1], 3])
+print(fun_sum([[1, 2, [3]], [1], 3]))
 
 
 
